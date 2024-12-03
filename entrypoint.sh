b3e6984ea823bd67719dd77de6ef449c949bb741
Mrudula <mrudulaacharya18@gmail.com>
2024-12-03 15:04:31 -0500

    CI/CD

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,6 @@ airflow db init
 # Create an Airflow user (if not already created)
 airflow users create --username smp1699 --firstname Mrudula --lastname Acharya --role Admin --email mrudulaacharya18@gmail.com --password smp1699
 
-# # Ensure Git is initialized and synced for DVC
-# if [ ! -d "/opt/airflow/.git" ]; then
-#   echo "Initializing Git repository for DVC..."
-#   git init
-#   git config --global user.name "Airflow DVC Logger"
-#   git config --global user.email "shalakapkar@gmail.com"
-#   git add .
-#   git commit -m "Initial commit for Airflow and DVC setup"
-# fi
-# Start the scheduler in the background
-# chmod -R 777 /opt/airflow/logs
-# chown -R airflow:airflow /opt/airflow/logs
-# chown -R airflow:airflow /opt/airflow/dags
 
 chmod -R 777 /opt/airflow/log
 airflow scheduler &
