cb8f8ea6d71fdbcf917dd61ea8cb191b4a0b4691
Mrudula <mrudulaacharya18@gmail.com>
2024-12-03 16:00:34 -0500

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-
+# Set permissions for the logs directory
+chmod -R 775 /opt/airflow/logs
+chown -R airflow:airflow /opt/airflow/logs
 # Initialize the Airflow database
 airflow db init
 
 # Create an Airflow user (if not already created)
 airflow users create --username smp1699 --firstname Mrudula --lastname Acharya --role Admin --email mrudulaacharya18@gmail.com --password smp1699
-# Set permissions for the logs directory
-chmod -R 777 /opt/airflow/logs
+
+
 airflow scheduler &
 
 sleep 60
