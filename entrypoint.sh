90497a874a29f030b6a9cb31768d37279428f941
Mrudula <mrudulaacharya18@gmail.com>
2024-12-02 19:11:10 -0500

    CI/CD

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-# Initialize DVC if not already done
-# if [ ! -d "/opt/airflow/.dvc" ]; then
-#   echo "Initializing DVC..."
-#   dvc init
-# fi
-
-# # Add datasets to DVC tracking
-# if [ -d "/opt/airflow/raw_data" ]; then
-#   dvc add /opt/airflow/raw_data
-# fi
-
-# if [ -d "/opt/airflow/motor_assessments" ]; then
-#   dvc add /opt/airflow/motor_assessments
-# fi
-# # Log the cleaned data if it exists
-# if [ -f "/opt/airflow/outputs/airflow_cleaned_data.csv" ]; then
-#   dvc add /opt/airflow/outputs/airflow_cleaned_data.csv
-# fi
-
-# # Commit DVC changes to Git
-# git add /opt/airflow/raw_data.dvc /opt/airflow/motor_assessments.dvc
-# git commit -m "Track data using DVC"
 
 
 # Initialize the Airflow database
